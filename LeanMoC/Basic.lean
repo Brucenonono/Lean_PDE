c5d63c971da04ccc2bacc015d831cd3bb7ada67d
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-07-17 16:28:37 +0100

    fix notation

--- LeanMoC/Basic.lean
+++ LeanMoC/Basic.lean
@@ -2,27 +2,30 @@ import Mathlib
 
 variable {E : Type} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E]
 
-local notation "E'" => WithLp 2 (E × ℝ)
+variable (E) in
+abbrev timesℝ : Type := WithLp 2 (E × ℝ)
+
+postfix:max "xℝ" => timesℝ
 
 variable
-  (u : E' → ℝ)
+  (u : E xℝ → ℝ)
 
 #check fderiv ℝ u -- more commonly used in mathlib, more lemmas
 #check gradient u -- easier to visualize
 
 -- for now let's try with `gradient`, might switch later
 
-variable (U : Set E')
+variable (U : Set (E xℝ))
 
-variable (F : E' × ℝ × E' → ℝ)
+variable (F : (E xℝ) × ℝ × (E xℝ) → ℝ)
 
 -- we are interested in the following ODE
-#check ∀ x ∈ U, F (gradient u x) (u x) x = 0
+#check ∀ x ∈ U, F (gradient u x, u x, x) = 0
 
 -- let's write down the characteristic equations
 
-variable (p : ℝ → E') (z : ℝ → ℝ)
-  (x : ℝ → E') -- hopefully the output of the `x` function lives in `U`
+variable (p : ℝ → E xℝ) (z : ℝ → ℝ)
+  (x : ℝ → E xℝ) -- hopefully the output of the `x` function lives in `U`
 
-variable (x₀ : E')
+variable (x₀ : E xℝ)
 #check (gradient F x₀).1 -- D_pF in Evans
